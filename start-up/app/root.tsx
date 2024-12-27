1255434733d75ce0760c233c60cb275e0479b10b
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-27 16:55:34 +0530

    Update root.tsx

--- start-up/app/root.tsx
+++ start-up/app/root.tsx
@@ -47,8 +47,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <ThemeProvider>
+      {/* Navbar component */}
       <Navbar />
+      {/* Outlet for nested routes */}
       <Outlet />
+      {/* Footer component */}
       <Footer />
     </ThemeProvider>
   );
